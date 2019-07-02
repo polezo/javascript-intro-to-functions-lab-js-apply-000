7752b465b8d85df1ac3d3d9b84eabbc931385332
candescent-constraint-5633 <atyreefinch@gmail.com>
2019-07-02 00:02:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const logWhisper = string => {
 }
 
 const sayHiToGrandma = string => {
-  if (string === string.toLowerCase) {
+  if (string === string.toLowerCase()) {
     return "I can't hear you!";
-  } else if (string === string.toUpperCase) {
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
   } else if (string === "I love u, Grandma") {
     return "I love you, too."
